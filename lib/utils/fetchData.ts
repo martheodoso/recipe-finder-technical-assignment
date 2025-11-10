8075c615e5c99729cb7386af35c3a4e2cefd182c
Maria The <maria@MacBook-Air.local>
2025-11-10 21:04:52 +0000

    Add force cache

--- lib/utils/fetchData.ts
+++ lib/utils/fetchData.ts
@@ -7,7 +7,7 @@ export const fetchMealsByArea = async () => {
   const api = process.env.API;
   try {
     const response = await fetch(`${api}/list.php?a=list`, 
-			{ next: { revalidate: 3600 } });
+			{cache: "force-cache", next: { revalidate: 3600 } });
     if(!response.ok)
 			throw new Error("Failed to fetch the list of areas!!")
     return await response.json();
@@ -24,8 +24,9 @@ export const fetchMealsByArea = async () => {
 export const fetchMealsByCuisine = async () => {
   const api = process.env.API;
 	try {
+    
     const response = await fetch(`${api}/list.php?c=list`, 
-			{ next: { revalidate: 3600 } });
+			{ cache: "force-cache", next: { revalidate: 3600 } });
     if(!response.ok)
 			throw new Error("Failed to fetch the list of cuisines!!")
     return await response.json();
@@ -44,7 +45,7 @@ export const fetchDataByFirstLetter = async (letter: string) => {
 	const api = process.env.API;
 	try {
     const response = await fetch(`${api}/search.php?f=${letter}`, 
-			{ next: { revalidate: 3600 } });
+			{ cache: "force-cache", next: { revalidate: 3600 } });
     if(!response.ok)
 			throw new Error("Failed to fetch the data when filtering by first letter!!")
     return await response.json();
@@ -63,7 +64,7 @@ export const fetchDataByIngredient = async (ingredient: string) => {
 	const api = process.env.API;
 	try {
     const response = await fetch(`${api}/filter.php?i=${ingredient}`, 
-			{ next: { revalidate: 3600 } });
+			{cache: "force-cache", next: { revalidate: 3600 } });
     if(!response.ok)
 			throw new Error("Failed to fetch the data when filtering by ingredient!!")
     return await response.json();
@@ -82,7 +83,7 @@ export const fetchDataById = async (id: string) => {
   const api = process.env.API;
   try {
     const response = await fetch(`${api}/lookup.php?i=${id}`, 
-			{ next: { revalidate: 3600 } });
+			{cache: "force-cache", next: { revalidate: 3600 } });
     if(!response.ok)
 			throw new Error("Failed to fetch the data when filtering by ingredient!!")
     return await response.json();
